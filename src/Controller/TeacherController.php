e90890788057e9b19ce0ba9dc3cbce78bfcc4322
LindaOuer <linda.ouerfelli@gmail.com>
2022-09-22 14:18:33 +0100

    Controller - Routing: TeacherController

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -15,4 +15,17 @@ class TeacherController extends AbstractController
             'controller_name' => 'TeacherController',
         ]);
     }
+    #[Route('/showTeacher/{name}', name: 'teacher_show')]
+    public function showTeacher($name): Response
+    {
+        return $this->render('teacher/showTeacher.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/showStudents', name: 'teacher_showStudents')]
+    public function showStudents(): Response
+    {
+        return $this->redirectToRoute('student_index');
+    }
 }
