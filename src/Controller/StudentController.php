20bc9a21fb89d1c311ce8bbc81e0f4d27ff10989
LindaOuer <linda.ouerfelli@gmail.com>
2022-09-22 13:45:51 +0100

    Example: Redirect - Request

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,9 +17,24 @@ class StudentController extends AbstractController
         return new Response('Hello index!');
     }
 
-    #[Route("/example",name:"student_example")]
+    #[Route("/example/",name:"student_example")]
     public function example(): Response
     {
         return new Response('Hello example!');
+    }
+
+    #[Route('/exampleRequest', name:'student_exampleRequest')]
+    public function exampleRequest(Request $request):Response {
+        return new Response($request->attributes->get('_route'));
+    }
+
+    #[Route('/exampleRedirect', name:'student_exampleRedirect')]
+    public function exampleRedirect():RedirectResponse {
+        return $this->redirect('https://www.google.com');
+    }
+
+    #[Route('/exampleRedirectToRoute', name:'student_exampleRedirectToRoute')]
+    public function exampleRedirectToRoute():RedirectResponse {
+        return $this->redirectToRoute("index_page");
     }
 }
